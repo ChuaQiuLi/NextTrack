a2d571904ca92e5ef7fd88c1487b41f37e46f1db
Qiu Li <qiulichua@gmail.com>
2026-08-01 23:10:19 +0800

    improve rapidapi track analysis

--- backend/services/cache.js
+++ backend/services/cache.js
@@ -24,27 +24,3 @@ function get(key){
 }
 
 module.exports = { get, set };
-
-
-// const cache = {};
-
-// function set(key, value) {
-//   cache[key] = {
-//     data: value,
-//     expiry: Date.now() + 1000 * 60 * 10 
-//   };
-// }
-
-// function get(key) {
-//   const item = cache[key];
-//   if (!item) return null;
-
-//   if (Date.now() > item.expiry) {
-//     delete cache[key];
-//     return null;
-//   }
-
-//   return item.data;
-// }
-
-// module.exports = { get, set };
